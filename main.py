463de3a9bb5e9a2c4afdc977253b6651c5baf208
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-05-30 13:39:40 -0700

    threads

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ import time
 from datetime import datetime
 import threading
 from envirophat import light, weather
+import csv
 
+
+# data collection
+dataFile = 'envirophat_data.csv'
+fields = ['Date/time', 'Temperature', 'Lighting']
+
+def writing():
+	with open(dataFile, mode='w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow(['Date-Time', 'Temperature', 'Lighting'])
 
 # collect date/time data
 def dateTime():
 	while True:
+		global now 
 		now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		print(f'Time logged: {now}')
+		print(f'Time: {now}')
 		time.sleep(1)
 
 
@@ -16,6 +27,7 @@ def dateTime():
 # collect light data
 def lightData():
 	while True:
+		global lighting 
 		lighting = light.light()
 		print(f'Light level: {lighting}')
 		time.sleep(1)
@@ -24,13 +36,38 @@ def lightData():
 # collect temperature data
 def tempData():
 	while True:
+		global temp 
 		temp = weather.temperature()
 		print(f'Temperature: {temp}')
 		time.sleep(1)
 
 
 # collect data
-def collectData():
-		
+def dataCollect():
+	
+	while True:
+		with open(dataFile, mode='a', newline='') as file:
+			writer = csv.writer(file)
+			writer.writerow([now, temp, lighting])
 
-collect_data()
+if __name__ == "__main__":
+	
+	writing()
+	
+	# thread creation
+	timeThread = threading.Thread(target=dateTime)
+	tempThread = threading.Thread(target=tempData)
+	lightThread = threading.Thread(target=lightData)
+	dataThread = threading.Thread(target=dataCollect)
+	
+	# run threads
+	timeThread.start()
+	tempThread.start()
+	lightThread.start()
+	dataThread.start()
+	
+	# join threads to main
+	timeThread.join()
+	tempThread.join()
+	lightThread.join()
+	dataThread.join()
