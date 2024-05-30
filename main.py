d07f591c3296037a5e303092e9a6ef44d394608b
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-05-30 12:19:16 -0700

    building

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-from envirophat import light
+import time
+from datetime import datetime
+import threading
+from envirophat import light, weather
 
-print ('pass')
+
+# collect date/time data
+def dateTime():
+	while True:
+		now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		print(f'Time logged: {now}')
+		time.sleep(1)
+
+
+
+# collect light data
+def lightData():
+	while True:
+		lighting = light.light()
+		print(f'Light level: {lighting}')
+		time.sleep(1)
+	
+
+# collect temperature data
+def tempData():
+	while True:
+		temp = weather.temperature()
+		print(f'Temperature: {temp}')
+		time.sleep(1)
+
+
+# collect data
+def collectData():
+		
+
+collect_data()
